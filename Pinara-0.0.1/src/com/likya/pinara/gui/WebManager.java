e556c96a82637c30cf048ecd12ec8710dbd1c3fd
hulagu <serkan.tas@likyateknoloji.com>
2015-02-20 00:25:17 +0200

    BasicAuth compatibility

--- Pinara-0.0.1/src/com/likya/pinara/gui/WebManager.java
+++ Pinara-0.0.1/src/com/likya/pinara/gui/WebManager.java
@@ -4,12 +4,17 @@ import java.net.InetSocketAddress;
 import java.util.HashMap;
 
 import com.likya.pinara.Pinara;
+import com.likya.pinara.model.PinaraAuthorization;
 import com.likya.pinara.rest.FlexHandler;
 import com.likya.pinara.rest.GraphViewHandler;
 import com.likya.pinara.rest.LogViewHandler;
 import com.likya.pinara.rest.RestfulHandler;
 import com.likya.pinara.utils.PasswordService;
+import com.sun.net.httpserver.Authenticator;
 import com.sun.net.httpserver.BasicAuthenticator;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
 public class WebManager {
@@ -25,7 +30,7 @@ public class WebManager {
 	private String sessionUserName;
 	private String sessionPassWord;
 
-	private HashMap<String, Authorization> authorizationList;
+	private HashMap<String, PinaraAuthorization> authorizationList;
 
 	MyAuthenticator myAuthenticator;
 
@@ -39,6 +44,55 @@ public class WebManager {
 
 		public MyAuthenticator(String realm) {
 			super(realm);
+		}
+
+//		public Result authenticate(HttpExchange t) {
+//			HttpContext context = t.getHttpContext();
+//			Headers rmap = (Headers) t.getRequestHeaders();
+//			/*
+//			 * look for auth token
+//			 */
+//			String auth = rmap.getFirst("Authorization");
+//			if (auth == null) {
+//				Headers map = (Headers) t.getResponseHeaders();
+//				map.set("WWW-Authenticate", "Basic realm=" + "\"" + realm + "\"");
+//				return new Authenticator.Retry(401);
+//			}
+//			int sp = auth.indexOf(' ');
+//			if (sp == -1 || !auth.substring(0, sp).equals("Basic")) {
+//				return new Authenticator.Failure(401);
+//			}
+//			byte[] b = Base64.base64ToByteArray(auth.substring(sp + 1));
+//			String userpass = new String(b);
+//			int colon = userpass.indexOf(':');
+//			String uname = userpass.substring(0, colon);
+//			String pass = userpass.substring(colon + 1);
+//
+//			if (checkCredentials(uname, pass)) {
+//				return new Authenticator.Success(new HttpPrincipal(uname, realm));
+//			} else {
+//				/* reject the request again with 401 */
+//
+//				Headers map = (Headers) t.getResponseHeaders();
+//				map.set("WWW-Authenticate", "Basic realm=" + "\"" + realm + "\"");
+//				return new Authenticator.Failure(401);
+//			}
+//		}
+
+		public Result authenticate(HttpExchange arg0) {
+			
+			Headers rmap = (Headers) arg0.getRequestHeaders();
+			String auth = rmap.getFirst("Authorization");
+			
+			System.out.println(auth);
+			
+			Result superResult = super.authenticate(arg0);
+
+			if (!(superResult instanceof Authenticator.Failure)) {
+				return superResult;
+			}
+
+			return new Authenticator.Retry(400);
 		}
 
 		public boolean checkCredentials(String username, String password) {
@@ -68,21 +122,20 @@ public class WebManager {
 		HttpServer server;
 		server = HttpServer.create(new InetSocketAddress(hostName, httpPort), 5);
 
-		//HttpContext flexContext = 
-				server.createContext("/" + FLEX_CTX, new FlexHandler());
-		// Pinara.getLogger().info(flexContext.getPath());
- 
-		// HttpContext restfulContext = 
-				server.createContext("/" + RESTFUL_CTX, new RestfulHandler());
+		HttpContext flexContext = server.createContext("/" + FLEX_CTX, new FlexHandler());
+		Pinara.getLogger().info(flexContext.getPath());
 
-		// HttpContext logDetailContext = 
-				server.createContext("/" + LOGVIEW_CTX, new LogViewHandler());
-		// HttpContext graphViewContext =
-				server.createContext("/" + GRAPHVIEW_CTX, new GraphViewHandler());
-				
-//		TODO Later !!!!
-//		MyAuthenticator myAuthenticator = new MyAuthenticator("pinara");
-//		restfulContext.setAuthenticator(myAuthenticator);
+		HttpContext restfulContext = server.createContext("/" + RESTFUL_CTX, new RestfulHandler());
+
+		HttpContext logDetailContext = server.createContext("/" + LOGVIEW_CTX, new LogViewHandler());
+		HttpContext graphViewContext = server.createContext("/" + GRAPHVIEW_CTX, new GraphViewHandler());
+
+		MyAuthenticator myAuthenticator = new MyAuthenticator("pinara");
+
+		//flexContext.setAuthenticator(myAuthenticator);
+		restfulContext.setAuthenticator(myAuthenticator);
+		logDetailContext.setAuthenticator(myAuthenticator);
+		graphViewContext.setAuthenticator(myAuthenticator);
 
 		server.setExecutor(null); // creates a default executor
 		server.start();

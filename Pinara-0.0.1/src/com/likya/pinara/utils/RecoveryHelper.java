44eb0d1bb0a51ec19afdbcbec62bfd6938a818d7
hulagu <serkan.tas@likyateknoloji.com>
2015-03-19 22:57:08 +0200

    .

---
+++ Pinara-0.0.1/src/com/likya/pinara/utils/RecoveryHelper.java
@@ -0,0 +1,25 @@
+package com.likya.pinara.utils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.likya.myra.jef.ConfigurationManagerImpl;
+import com.likya.myra.jef.utils.MyraPersistApi;
+
+public class RecoveryHelper {
+	
+	public static boolean isInRecoveryState() {
+
+		boolean checkValue = false;
+
+		checkValue = MyraPersistApi.getMyraConfig(false).getPersistent();
+		
+		Path persistFile = Paths.get(ConfigurationManagerImpl.fileToPersist);
+		
+		checkValue = Files.exists(persistFile);
+
+		return checkValue;
+	}
+	
+}

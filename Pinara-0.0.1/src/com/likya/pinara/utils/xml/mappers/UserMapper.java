69c65f4d479719badaa9f0f33e2b7d06c18df0f7
hulagu <serkan.tas@likyateknoloji.com>
2015-06-09 23:38:45 +0300

    .

--- Pinara-0.0.1/src/com/likya/pinara/utils/xml/mappers/UserMapper.java
+++ Pinara-0.0.1/src/com/likya/pinara/utils/xml/mappers/UserMapper.java
@@ -1,13 +1,41 @@
 package com.likya.pinara.utils.xml.mappers;
 
+import java.util.ArrayList;
+
 import org.apache.xmlbeans.XmlCursor;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
 
 import com.likya.pinara.model.User;
+import com.likya.pinara.model.User.RoleInfo;
 
 public class UserMapper {
 
+	public static String getMapped(ArrayList<User> userList) {
+	
+		String retValue = "";
+		
+		XmlObject xmlObject = XmlObject.Factory.newInstance();
+		XmlCursor xmlCursor = xmlObject.newCursor();
+		xmlCursor.toEndToken();
+		
+		xmlCursor.beginElement("userList");
+		
+		for(User user : userList) {
+			addUserInfo(xmlCursor, user);
+		}
+		
+		try {
+			XmlOptions saveOptions = new XmlOptions().setSavePrettyPrint().setSavePrettyPrintIndent(2);
+			retValue = xmlObject.xmlText(saveOptions);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return retValue;
+		
+	}
+	
 	public static String getMapped(User user) {
 
 		String retValue = "";
@@ -43,10 +71,42 @@ public class UserMapper {
 		xmlCursor.beginElement("username");
 		xmlCursor.insertChars("" + user.getUsername());
 		xmlCursor.toNextToken();
+		
+		xmlCursor.beginElement("password");
+		xmlCursor.insertChars("" + user.getPassword());
+		xmlCursor.toNextToken();
 
 		xmlCursor.beginElement("roleinfo");
 		xmlCursor.insertChars("" + user.getRoleInfo());
 		xmlCursor.toNextToken();
 
+	}
+	
+	public static User parseUser(String userXml) {
+		
+		String myraGenericJob = userXml.split("<userInfo>")[1].split("</userInfo>")[0];
+		
+		String username = myraGenericJob.split("<username>")[1].split("</username>")[0];
+		String password = "";
+		if(myraGenericJob.contains("<password>")) {
+			password =  myraGenericJob.split("<password>")[1].split("</password>")[0];
+		}
+		String roleinfo = myraGenericJob.split("<roleinfo>")[1].split("</roleinfo>")[0];
+		
+		User testUser = null;
+		try {
+			testUser = new User(RoleInfo.valueOf(roleinfo), username, password);
+			String id = "";
+			if(myraGenericJob.contains("<id>")) {
+				id =  myraGenericJob.split("<id>")[1].split("</id>")[0];
+				testUser.setId(Integer.parseInt(id));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return testUser;
+		
 	}
 }

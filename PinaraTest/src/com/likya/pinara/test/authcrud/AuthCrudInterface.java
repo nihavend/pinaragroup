9d2bfb919bb0f2bf035c584cd3e8d3dfe433b3ec
hulagu <serkan.tas@likyateknoloji.com>
2015-06-09 23:44:51 +0300

    .

---
+++ PinaraTest/src/com/likya/pinara/test/authcrud/AuthCrudInterface.java
@@ -0,0 +1,19 @@
+package com.likya.pinara.test.authcrud;
+
+
+public interface AuthCrudInterface {
+
+	public void testSimpleAdd();
+
+	public void testSimpleGetUserList();
+	
+	public void testSimpleReadWithId();
+
+	public void testSimpleReadWithUsername();
+
+	public void testSimpleUpdate();
+	
+	public void testSimpleDelete();
+	
+	public void testSimpleDeleteWithId();
+}

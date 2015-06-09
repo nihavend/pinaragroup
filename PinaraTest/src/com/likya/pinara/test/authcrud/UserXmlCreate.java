9f48a1d1f416d97f4c70c95c16648ff9b98fcfa5
hulagu <serkan.tas@likyateknoloji.com>
2015-06-09 11:41:04 +0300

    .

---
+++ PinaraTest/src/com/likya/pinara/test/authcrud/UserXmlCreate.java
@@ -0,0 +1,31 @@
+package com.likya.pinara.test.authcrud;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.likya.pinara.model.User;
+import com.likya.pinara.model.User.RoleInfo;
+import com.likya.pinara.utils.xml.mappers.UserMapper;
+
+
+public class UserXmlCreate extends TestCase {
+	
+	private User sampleUser;
+	
+	protected void setUp() {
+		try {
+			sampleUser = new User(RoleInfo.ADMIN, "pinara", "pinara");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+	public void testCreateUserXml() {
+		
+		String xmlStrUser = UserMapper.getMapped(sampleUser);
+		
+		Assert.assertNotNull(xmlStrUser);
+		
+	}
+
+}

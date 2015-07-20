e732a3494aa9bade0f0794cb3a2cb6026b8126fc
hulagu <serkan.tas@likyateknoloji.com>
2015-07-20 18:21:01 +0300

    .

--- Pinara-0.0.1/bin/start-no-tr.bat
+++ Pinara-0.0.1/bin/start-no-tr.bat
@@ -1,3 +1,7 @@
-java -Duser.country=ENTR -Duser.language=entr -jar pinara-0.0.1.jar> nohup.out 2> nohup.err
+type README.txt
+@echo off
+Chcp 1254
+#java -Duser.country=ENTR -Duser.language=entr -jar pinara-0.0.1.jar> nohup.out 2> nohup.err
+java -Duser.country=ENTR -Duser.language=entr -jar pinara-0.0.1.jar 2> nohup.err
 
 
--- Pinara-0.0.1/bin/start.sh
+++ Pinara-0.0.1/bin/start.sh
@@ -1,3 +1,5 @@
-java -jar pinara-0.0.1.jar >nohup.out 2> nohup.err
+cat README.txt
+!java -jar pinara-0.0.1.jar >nohup.out 2> nohup.err
+java -jar pinara-0.0.1.jar 2> nohup.err
 
 

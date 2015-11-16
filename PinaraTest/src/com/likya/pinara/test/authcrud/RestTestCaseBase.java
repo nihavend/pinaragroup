fc208f58d579e618ca1f2decbc7263487dddbc5c
hulagu <serkan.tas@likyateknoloji.com>
2015-11-16 22:24:16 +0200

    .

--- PinaraTest/src/com/likya/pinara/test/authcrud/RestTestCaseBase.java
+++ PinaraTest/src/com/likya/pinara/test/authcrud/RestTestCaseBase.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.apache.commons.codec.binary.Base64;
-
 import junit.framework.TestCase;
+
+import org.apache.commons.codec.binary.Base64;
 
 
 public abstract class RestTestCaseBase extends TestCase {
@@ -21,6 +22,23 @@ public abstract class RestTestCaseBase extends TestCase {
 //	private final String LOGVIEW_CTX = "logview";
 //	public final static String GRAPHVIEW_CTX = "graphview";
 //	public final static String RESTFUL_CTX = "flex/restsrvc";
+	
+	private String prepareReqProp() {
+	
+		String name = "pinara";
+		String password = "pinara";
+
+		String authString = name + ":" + password;
+		
+		// System.out.println("auth string: " + authString);
+		byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+		String authStringEnc = new String(authEncBytes);
+		// System.out.println("Base64 encoded auth string: " + authStringEnc);
+		
+		String retValue = "Basic " + authStringEnc;
+		
+		return retValue;
+	}
 
 	protected String httpPost(String requestUrl, String urlParameters) {
 		
@@ -31,18 +49,9 @@ public abstract class RestTestCaseBase extends TestCase {
 		
 		try {
 			
-			String name = "pinara";
-			String password = "pinara";
-
-			String authString = name + ":" + password;
-			// System.out.println("auth string: " + authString);
-			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
-			String authStringEnc = new String(authEncBytes);
-			// System.out.println("Base64 encoded auth string: " + authStringEnc);
-			
 			URL url = new URL(requestUrl);
 		    URLConnection urlConnection = url.openConnection();
-		    urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+		    urlConnection.setRequestProperty("Authorization", prepareReqProp());
 		    urlConnection.setDoOutput(true);
 		    OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
 
@@ -67,5 +76,31 @@ public abstract class RestTestCaseBase extends TestCase {
 		}
 		
 		return retStr.toString();
+	}
+	
+	
+	protected String httpGet(String urlToRead, String urlParameters) {
+
+		StringBuilder result = new StringBuilder();
+
+		try {
+			URL url = new URL(urlToRead);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			
+			conn.setRequestProperty("Authorization", prepareReqProp());
+			conn.setRequestMethod("GET");
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			rd.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result.toString();
 	}
 }

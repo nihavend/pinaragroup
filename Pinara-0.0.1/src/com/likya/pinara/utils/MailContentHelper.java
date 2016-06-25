a1dcf63e894a9bb426014f43d32031acb480b127
hulagu <serkan.tas@likyateknoloji.com>
2016-06-26 01:09:14 +0300

    https://likyateknoloji.atlassian.net/browse/PINARA-239

--- Pinara-0.0.1/src/com/likya/pinara/utils/MailContentHelper.java
+++ Pinara-0.0.1/src/com/likya/pinara/utils/MailContentHelper.java
@@ -55,33 +55,34 @@ public class MailContentHelper {
 
 		StringBuilder stringBuilder = new StringBuilder();
 
-		stringBuilder.append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\"http://www.w3.org/tr/html4/loose.dtd\">");
+		stringBuilder.append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/tr/html4/loose.dtd\">");
 		stringBuilder.append("<html>");
 		stringBuilder.append("    <head>");
 		stringBuilder.append("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>");
-		stringBuilder.append("        <title>Tlos Scheduler</title>");
+		stringBuilder.append("        <title>Pınara Scheduler</title>");
 		stringBuilder.append("    </head>");
 		stringBuilder.append("    <body>");
 		stringBuilder.append("        <table border=\"0\" summary=\"" + Pinara.getMessage("JobQueueOperations.44") + "\" align=\"center\">"); //$NON-NLS-3$
 		stringBuilder.append("            <tr>");
 		stringBuilder.append("               <td>");
 		stringBuilder.append("                    <img src=\"cid:likyajpg10976@likyateknoloji.com\" align=\"right\" width=\"100\" height=\"35\" alt=\"\"/>");
-		stringBuilder.append("                    <tr align=\"right\">");
-		stringBuilder.append("                        <td>");
-		String ipAddress = Pinara.getInstance().getConfigurationManager().getPinaraConfig().getServerIpAddress();
-		int port = Pinara.getInstance().getConfigurationManager().getPinaraConfig().getHttpPort();
-		stringBuilder.append("                            <a href=\"http://" + ipAddress + ":" + port + "/\" target=\"_blank\" title=\"" + Pinara.getMessage("JobQueueOperations.54") + "\">" + Pinara.getMessage("JobQueueOperations.56") + "</a>");
-		stringBuilder.append("                        </td>");
-		stringBuilder.append("                    </tr>");
 		stringBuilder.append("                </td>");
 		stringBuilder.append("            </tr>");
+		stringBuilder.append("            <tr align=\"right\">");
+		stringBuilder.append("                <td>");
+		String ipAddress = Pinara.getInstance().getConfigurationManager().getPinaraConfig().getServerIpAddress();
+		int port = Pinara.getInstance().getConfigurationManager().getPinaraConfig().getHttpPort();
+		stringBuilder.append("                      <a href=\"http://" + ipAddress + ":" + port + "/\" target=\"_blank\" title=\"" + Pinara.getMessage("JobQueueOperations.54") + "\">" + Pinara.getMessage("JobQueueOperations.56") + "</a>");
+		stringBuilder.append("                </td>");
+		stringBuilder.append("            </tr>");
+
 		stringBuilder.append("            <tr>");
 		stringBuilder.append("                <td align=\"center\">");
 
 		if (localizedMessage == null) {
-			stringBuilder.append("                    <h1>TLOS Scheduler</h1>" + Pinara.getMessage("JobQueueOperations.64"));
+			stringBuilder.append("                    <h1>Pınara Scheduler</h1>" + Pinara.getMessage("JobQueueOperations.64"));
 		} else {
-			stringBuilder.append("                    <h1>TLOS Scheduler</h1>" + localizedMessage);
+			stringBuilder.append("                    <h1>Pınara Scheduler</h1>" + localizedMessage);
 		}
 
 		stringBuilder.append("                </td>");
@@ -98,16 +99,18 @@ public class MailContentHelper {
 		stringBuilder.append("                            <em>" + Pinara.getMessage("JobQueueOperations.78") + "</em>"); //$NON-NLS-3$
 		stringBuilder.append("                        </caption>");
 		stringBuilder.append("				        <tr>");
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.82"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.84"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.86"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.88"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.90"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.92"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.94"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.96"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.98"));
-		stringBuilder.append("				        <th>" + Pinara.getMessage("JobQueueOperations.100"));
+		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.82") + "</th>");
+		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.84") + "</th>");
+		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.86") + "</th>");
+		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.88") + "</th>");
+		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.90") + "</th>");
+//		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.92") + "</th>");
+//		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.94") + "</th>");
+//		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.96") + "</th>");
+//		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.98") + "</th>");
+//		stringBuilder.append("				        	<th>" + Pinara.getMessage("JobQueueOperations.100") + "</th>");
+		stringBuilder.append("			 			</tr>");
+
 
 		Iterator<JobImpl> jobsIterator = jobQueue.values().iterator();
 
@@ -117,25 +120,23 @@ public class MailContentHelper {
 			AbstractJobType abstractJobType = scheduledJob.getAbstractJobType();
 			
 			stringBuilder.append("				        <tr>");
-			stringBuilder.append("				        <td>" + abstractJobType.getBaseJobInfos().getJsName());
-			stringBuilder.append("				        <td>" + abstractJobType.getBaseJobInfos().getJobTypeDetails().getJobCommand());
-			stringBuilder.append("				        <td>" + abstractJobType.getBaseJobInfos().getJobLogPath());
+			stringBuilder.append("				        	<td>" + abstractJobType.getBaseJobInfos().getJsName() + "</td>");
+			stringBuilder.append("				        	<td>" + abstractJobType.getBaseJobInfos().getJobTypeDetails().getJobCommand() + "</td>");
+			stringBuilder.append("				        	<td>" + abstractJobType.getBaseJobInfos().getJobLogPath() + "</td>");
 			// TODO stringBuilder.append("				        <td>" + jobProperties.getPreviousTime());
 
 			TimeManagement timeManagement = abstractJobType.getManagement().getTimeManagement();
 			
 			if(timeManagement != null) {
-				stringBuilder.append("				        <td>" + abstractJobType.getManagement().getTimeManagement().getJsPlannedTime());
-				stringBuilder.append("				        <td>" + abstractJobType.getManagement().getTimeManagement().getJsRealTime());
+				stringBuilder.append("				        <td>" + abstractJobType.getManagement().getTimeManagement().getJsPlannedTime().getStartTime() + "</td>");
+				stringBuilder.append("				        <td>" + (abstractJobType.getManagement().getTimeManagement().getJsRealTime() == null ? "-":abstractJobType.getManagement().getTimeManagement().getJsRealTime()) + "</td>");
 			} else {
-				stringBuilder.append("				        <td>");
-				stringBuilder.append("				        <td>");
+				stringBuilder.append("				        <td>-</td>");
+				stringBuilder.append("				        <td>-</td>");
 			}
 			
-			// TODO stringBuilder.append("				        <td>" + jobProperties.getWorkDuration());
-			// TODO stringBuilder.append("				        <td>" + (jobProperties.getTime() == null ? "-" : DateUtils.getDate(jobProperties.getTime())));
-			stringBuilder.append("				        <td>" + Pinara.getMessage("JobQueueOperations.111"));
-			// TODO stringBuilder.append("				        <td>" + extractList(jobProperties.getJobDependencyInfoList()));
+			stringBuilder.append("				        <tr>");
+			
 		}
 
 		stringBuilder.append("                    </table>");
@@ -166,7 +167,7 @@ public class MailContentHelper {
 		footerValue.append("        <hr size=\"1\" color=\"black\" width=\"80%\"/>");
 		footerValue.append("        <h6 align=\"center\">" + Pinara.getVersion());
 		footerValue.append("            <br/>" + Pinara.getMessage("JobQueueOperations.126"));
-		footerValue.append("            <br/>Ataşehir, İstanbul, Türkiye");
+		footerValue.append("            <br/>Tuzla, İstanbul, Türkiye");
 		footerValue.append("            <br/>");
 		footerValue.append("            <a href=\"http://www.likyateknoloji.com\" title=\"www.likyateknoloji.com\">www.likyateknoloji.com</a>&nbsp;&nbsp;");
 		footerValue.append("            <a href=\"mailto:bilgi@likyateknoloji.com\">bilgi@likyateknoloji.com</a>");

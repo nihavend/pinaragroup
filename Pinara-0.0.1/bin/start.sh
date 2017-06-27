effb9e5631bc20dbc186f4d31511aa906f5697c6
nihavend <serkan.tas@likyateknoloji.com>
2017-06-27 23:51:20 +0300

    .

--- Pinara-0.0.1/bin/start.sh
+++ Pinara-0.0.1/bin/start.sh
@@ -1,5 +1,4 @@
 cat README.txt
-!java -jar pinara-0.0.1.jar >nohup.out 2> nohup.err
-java -jar pinara-0.9.1.jar 2> nohup.err
+java -Duser.country=ENTR -Duser.language=entr -jar pinara-0.0.1.jar 2> error.log
 
 

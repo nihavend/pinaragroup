686141ca7de12d55103c23ee1bf8df253252eaec
nihavend <serkan.tas@likyateknoloji.com>
2017-10-29 02:24:10 +0300

    history.js added to export xml.

---
+++ Pinara-0.0.1/src/com/likya/pinara/utils/PersistDBApi.java
@@ -0,0 +1,60 @@
+package com.likya.pinara.utils;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.likya.myra.jef.core.CoreFactory;
+import com.likya.myra.jef.jobs.JobImpl;
+import com.likya.pinara.Pinara;
+import com.likya.pinara.dao.nodb.JobCrudNoDBDAO;
+import com.likya.xsd.myra.model.joblist.AbstractJobType;
+import com.likya.xsd.myra.model.joblist.JobListDocument;
+import com.likya.xsd.myra.model.stateinfo.LiveStateInfoDocument.LiveStateInfo;
+import com.likya.xsd.myra.model.stateinfo.LiveStateInfosDocument;
+import com.likya.xsd.myra.model.stateinfo.LiveStateInfosType;
+
+public class PersistDBApi {
+	
+	private static Path dataPath = FileSystems.getDefault().getPath(Pinara.DATA_PATH);
+
+	public static JobListDocument readJobs() {
+		JobListDocument jobListDocument = new JobCrudNoDBDAO().readJobs(dataPath);
+		return jobListDocument;
+	}
+	
+	public static boolean saveJob(AbstractJobType abstractJobType) {
+		return new JobCrudNoDBDAO().saveJob(dataPath, abstractJobType.getId(), abstractJobType.xmlText());
+	}
+	
+	public static boolean deleteJob(String jobId) {
+		return new JobCrudNoDBDAO().deleteJob(dataPath, jobId);
+	}
+	
+	public static boolean updateJobHist(String jobId, LiveStateInfosType liveStateInfosType) {
+
+		if(liveStateInfosType.getLiveStateInfoArray().length > Pinara.MAX_HISTROY_LEN) {
+			LiveStateInfosDocument liveStateInfosDocument = new JobCrudNoDBDAO().readJobHist(dataPath, jobId);
+			System.out.println(liveStateInfosDocument);
+			while(liveStateInfosType.getLiveStateInfoArray().length > Pinara.MAX_HISTROY_LEN) {
+				LiveStateInfo liveStateInfo = liveStateInfosType.getLiveStateInfoArray(liveStateInfosType.getLiveStateInfoArray().length - 1);
+				liveStateInfosDocument.getLiveStateInfos().insertNewLiveStateInfo(0).set(liveStateInfo);
+				liveStateInfosType.removeLiveStateInfo(liveStateInfosType.getLiveStateInfoArray().length - 1);
+			}
+			System.out.println(liveStateInfosDocument);
+			new JobCrudNoDBDAO().saveJobHist(dataPath, jobId, liveStateInfosDocument.xmlText());
+		}
+		return true;
+	}
+	
+	public static void handleJobDataChangeEvent(String jobId) {
+		// Yeni db yapısı sonrası eklendi
+		// İlk olarak değişen işi diske yansıtalım
+		// sonrasında da history bilgisi limiti aştıysa onu da diske yazalım
+		
+		JobImpl jobImpl = CoreFactory.getInstance().getMonitoringOperations().getJobQueue().get(jobId);
+		PersistDBApi.saveJob(jobImpl.getAbstractJobType());
+		LiveStateInfosType liveStateInfosType = jobImpl.getAbstractJobType().getStateInfos().getLiveStateInfos();
+		PersistDBApi.updateJobHist(jobId, liveStateInfosType);
+		
+	}
+}

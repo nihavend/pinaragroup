8450623bfbd8c8e4809bd30e5144f317c2e3ff87
nihavend <serkan.tas@likyateknoloji.com>
2017-11-09 22:27:25 +0300

    .

---
+++ myra-test-module-core/src/com/likya/myra/test/period/TestPeriodCalculations.java
@@ -0,0 +1,18 @@
+package com.likya.myra.test.period;
+
+import org.junit.Test;
+
+import com.likya.myra.jef.utils.timeschedules.PeriodCalculations;
+
+public class TestPeriodCalculations {
+
+	
+	@Test
+	public void addPeriod() {
+		
+		
+		
+		PeriodCalculations.forward(abstractJobType, messages);
+	}
+
+}

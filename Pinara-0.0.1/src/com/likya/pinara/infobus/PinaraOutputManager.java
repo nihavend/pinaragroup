a884b31db31ad67ef97ad956774528bd014ebc55
nihavend <serkan.tas@likyateknoloji.com>
2017-12-06 00:29:59 +0300

    mail structure reorganized

--- Pinara-0.0.1/src/com/likya/pinara/infobus/PinaraOutputManager.java
+++ Pinara-0.0.1/src/com/likya/pinara/infobus/PinaraOutputManager.java
@@ -1,6 +1,11 @@
 package com.likya.pinara.infobus;
 
+import java.util.HashMap;
+
 import com.likya.myra.commons.utils.LiveStateInfoUtils;
+import com.likya.myra.commons.utils.NetTreeResolver;
+import com.likya.myra.jef.core.CoreFactory;
+import com.likya.myra.jef.jobs.JobImpl;
 import com.likya.myra.jef.model.OutputData;
 import com.likya.pinara.Pinara;
 import com.likya.pinara.model.PinaraOutput;
@@ -21,6 +26,8 @@ public class PinaraOutputManager implements Runnable {
 	private boolean executePermission = true;
 	private PinaraOutput pinaraOutput = PinaraOutput.getInstance();
 	private boolean setForced = false;
+	
+	private Thread executorThread;
 
 	public PinaraOutputManager() {
 	}
@@ -40,9 +47,74 @@ public class PinaraOutputManager implements Runnable {
 		}
 	}
 
+	private void handle_JOBSTATECHANGE(OutputData outputData) {
+		
+		PersistDBApi.handleJobDataChangeEvent(outputData.getJobId());
+		
+		if(!setForced) {
+			// System.err.println(outputData.getLiveStateInfo());
+			// Pinara.getLogger().debug(outputData.getLiveStateInfo());
+			LiveStateInfo liveStateInfo = outputData.getLiveStateInfo();
+			String dump = "	Live State : " + (liveStateInfo.getStateName() == null ? "" : liveStateInfo.getStateName().toString());
+			dump += (liveStateInfo.getSubstateName() == null ? "" : ("-" + liveStateInfo.getSubstateName().toString()));
+			dump += (liveStateInfo.getStatusName() == null ? "" : ("-" + liveStateInfo.getStatusName().toString()));
+			Pinara.getLogger().debug(dump);
+			if(StateName.FINISHED.equals(outputData.getLiveStateInfo().getStateName())) {
+				Pinara.getLogger().debug("Job >> " + outputData.getJobId() + ":" + outputData.getJobName() + " is finished !!!!");
+				// System.err.println(outputData.getJobId() + " is finished !!!!");
+			}
+			
+			// TODO Resolve broadcasting type
+			// email, db, sms, etc
+			
+			PinaraConfig pinaraConfig = Pinara.getInstance().getConfigurationManager().getPinaraConfig();
+			if(pinaraConfig.isSetMailInfo()) {
+				MailInfo mailInfo = pinaraConfig.getMailInfo();
+				if(mailInfo.getEnabled()) {
+					LiveStateInfosType liveStateInfosType = mailInfo.getStateInfos().getLiveStateInfos();
+					if(LiveStateInfoUtils.containsAny(liveStateInfosType, liveStateInfo)) {
+						SimpleMail simpleMail = new SimpleMail("Job Durum değişikliği Job >> " + outputData.getJobId() + ":" + outputData.getJobName(), "Belirtilen iş şu duruma geçti : " + dump);
+						Pinara.getInstance().getConfigurationManager().getPinaraMailServer().sendMail(simpleMail);
+					}
+				}
+			}
+		}
+	}
+
+	private void handle_LOGANALYZER(OutputData outputData) {
+		PinaraConfig pinaraConfig = Pinara.getInstance().getConfigurationManager().getPinaraConfig();
+		if(pinaraConfig.isSetMailInfo()) {
+			MailInfo mailInfo = pinaraConfig.getMailInfo();
+			if(mailInfo.getEnabled()) {
+				Pinara.getInstance().getConfigurationManager().getPinaraMailServer().sendMail(new ContentMail("LOGANALYZER", outputData.getOutputContent().toString()));
+			}
+		}
+	}
+
+	private void handle_DEFAULT(OutputData outputData) {
+		Pinara.getLogger().error("handle_DEFAULT");
+	}
+
+	private void handle_ENDOFCYCLE(OutputData outputData) {
+		NetTreeResolver.NetTree netTree = (NetTreeResolver.NetTree) outputData.getOutputContent();
+		Pinara.getLogger().debug("Dependency group with virtualId  " + netTree.getVirtualId() + " is finished !!!!");
+		PinaraConfig pinaraConfig = Pinara.getInstance().getConfigurationManager().getPinaraConfig();
+		if(pinaraConfig.isSetMailInfo()) {
+			MailInfo mailInfo = pinaraConfig.getMailInfo();
+			if(mailInfo.getEnabled()) {
+				HashMap<String, JobImpl> jobMainQueue = CoreFactory.getInstance().getMonitoringOperations().getJobQueue();
+				HashMap<String, JobImpl> jobNetTreeQueue = new HashMap<String, JobImpl>();
+				for (String jobId : netTree.getMembers()) {
+					jobNetTreeQueue.put(jobId, jobMainQueue.get(jobId));
+				}
+				Pinara.getInstance().getConfigurationManager().getPinaraMailServer().sendMail(new EndOfCycleMail(netTree.getVirtualId(), jobNetTreeQueue));
+			}
+		}
+	}
+
 	public void run() {
 		Thread.currentThread().setName("PinaraOutputManager");
-		while (executePermission || pinaraOutput.getOutputList().size() > 0) {
+		while (!setForced && (executePermission || pinaraOutput.getOutputList().size() > 0)) {
 
 			while (pinaraOutput.getOutputList().size() > 0) {
 
@@ -51,37 +123,23 @@ public class PinaraOutputManager implements Runnable {
 					OutputData outputData = (OutputData) pinaraOutput.getOutputList().get(0);
 					Pinara.getLogger().debug("OutputManager işliyor ...");
 					
-					PersistDBApi.handleJobDataChangeEvent(outputData.getJobId());
-					
-					if(!setForced) {
-						// System.err.println(outputData.getLiveStateInfo());
-						// Pinara.getLogger().debug(outputData.getLiveStateInfo());
-						LiveStateInfo liveStateInfo = outputData.getLiveStateInfo();
-						String dump = "	Live State : " + (liveStateInfo.getStateName() == null ? "" : liveStateInfo.getStateName().toString());
-						dump += (liveStateInfo.getSubstateName() == null ? "" : ("-" + liveStateInfo.getSubstateName().toString()));
-						dump += (liveStateInfo.getStatusName() == null ? "" : ("-" + liveStateInfo.getStatusName().toString()));
-						Pinara.getLogger().debug(dump);
-						if(StateName.FINISHED.equals(outputData.getLiveStateInfo().getStateName())) {
-							Pinara.getLogger().debug("Job >> " + outputData.getJobId() + ":" + outputData.getJobName() + " is finished !!!!");
-							// System.err.println(outputData.getJobId() + " is finished !!!!");
-						}
-						
-						// TODO Resolve broadcasting type
-						// email, db, sms, etc
-						
-						PinaraConfig pinaraConfig = Pinara.getInstance().getConfigurationManager().getPinaraConfig();
-						
-						if(pinaraConfig.isSetMailInfo()) {
-							MailInfo mailInfo = pinaraConfig.getMailInfo();
-							if(mailInfo.getEnabled()) {
-								LiveStateInfosType liveStateInfosType = mailInfo.getStateInfos().getLiveStateInfos();
-								if(LiveStateInfoUtils.containsAny(liveStateInfosType, liveStateInfo)) {
-									SimpleMail simpleMail = new SimpleMail("Job Durum değişikliği Job >> " + outputData.getJobId() + ":" + outputData.getJobName(), "Belirtilen iş şu duruma geçti : " + dump);
-									Pinara.getInstance().getConfigurationManager().getPinaraMailServer().sendMail(simpleMail);
-								}
-							}
-						}
-					}
+					switch (outputData.getOutputType()) {
+					case JOBSTATECHANGE:
+						handle_JOBSTATECHANGE(outputData);
+						break;
+					case LOGANALYZER:
+						handle_LOGANALYZER(outputData);
+						break;
+					case BASIC:
+						handle_DEFAULT(outputData);
+						break;
+					case ENDOFCYCLE:
+						handle_ENDOFCYCLE(outputData);
+						break;
+					default:
+						Pinara.getLogger().error("Invalid output type :" + outputData.getOutputType());
+						break;
+					}			
 
 					if (!pinaraOutput.getOutputList().isEmpty()) {
 						pinaraOutput.getOutputList().remove(0);
@@ -97,8 +155,8 @@ public class PinaraOutputManager implements Runnable {
 				e.printStackTrace();
 			}
 		}
-		Pinara.getLogger().info(Pinara.getMessage("PinaraMailServer.7"));
-		Pinara.getLogger().info(Pinara.getMessage("PinaraMailServer.8") + pinaraOutput.getOutputList().size());
+		Pinara.getLogger().info(Pinara.getMessage("PinaraOutputManager kapatıldı"));
+		Pinara.getLogger().info("PinaraOutputManager : " + Pinara.getMessage("PinaraMailServer.8") + pinaraOutput.getOutputList().size());
 	}
 
 	public int getQueueSize() {
@@ -107,5 +165,13 @@ public class PinaraOutputManager implements Runnable {
 
 	public PinaraOutput getPinaraOutput() {
 		return pinaraOutput;
+	}
+
+	public Thread getExecutorThread() {
+		return executorThread;
+	}
+
+	public void setExecutorThread(Thread executorThread) {
+		this.executorThread = executorThread;
 	}
 }

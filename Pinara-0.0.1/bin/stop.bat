26b8809975405df7f0fb3bbde1129faa7722ca8b
sahin <sahin@macbookpro>
2018-06-22 21:59:44 +0300

    PINARA-356
    pınara stop script hazırlanması

---
+++ Pinara-0.0.1/bin/stop.bat
@@ -0,0 +1,5 @@
+@echo off
+Chcp 1254
+java -cp pinara-0.9.6.jar com.likya.pinara.PinaraRemoteManager -port 4004 -host localhost -terminate
+
+		
---
+++ Pinara-0.0.1/bin/stop.sh
@@ -0,0 +1,1 @@
+java -cp pinara-0.9.6.jar com.likya.pinara.PinaraRemoteManager -port 4004 -host localhost -terminate
